ed3363b1f5d38d8dd6bd1a094340fc21b4101f00
Muttu R K <kagalmutturaj@gmail.com>
2025-08-27 22:26:45 +0530

    Update MultipicationArrayEle.java

--- MultipicationArrayEle.java
+++ MultipicationArrayEle.java
@@ -12,6 +12,27 @@ class Solution {
             }
             return arr;
         }
+    O(1) SC
+    public static int[] productExceptSelf(int arr[]) {
+        int zero = 0, index = 0, pro = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                zero++;
+                index = i;
+            } else {
+                pro *= arr[i];
+            }
+        }
+        int[] res = new int[arr.length];
+        if (zero == 1) res[index] = pro;
+        else if (zero == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                res[i] = pro / arr[i];
+            }
+        }
+        return res;
+
+    }
 // rotate the array into right side
     public void rotate(int[] nums, int k) {
         int []arr=new int [nums.length];
